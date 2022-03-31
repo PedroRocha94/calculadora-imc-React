3b96e580d7cf99939c39570986801851b9a51297
Pedro Rocha <pedrorocha73658@gmail.com>
2022-03-31 10:45:11 -0300

    Update calculadora IMC

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
 import {useState} from 'react'
-import './app.css'
+import './app.css';
 
 export default function App(){
+
+  function calcularIMC(){
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if(imc < 18.6){
+      setMensagem("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2));
+    }else if(imc >= 18.6 && imc <24.9){
+      setMensagem("Peso ideal! Seu IMC: " + imc.toFixed(2));
+    }else if(imc >= 24.9 && imc <34.9){
+      setMensagem("Você está levemente acima do peso! Seu IMC: " + imc.toFixed(2));
+    }else if(imc > 34.9){
+      setMensagem("Cuidado obesidade! Seu IMC: " + imc.toFixed(2));
+    }
+  }
+
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
-
-
+  const [mensagem, setMensagem] = useState('');
+  
   return(
-    <div className="app">
-      <h1>Calculadora IMC</h1>
-      <span>Vamos calcular seu imc</span>
+    <div className='app'>
+      <h1>Calculadora de IMC</h1>
+      <span>Vamos calcular o seu IMC</span>
 
       <div className="area-input">
-        <input 
-          type="text"
-          placeholder="Peso em (Kg) Ex: 90"
-          value={peso}
-          onChange={(e)=> setPeso(e.target.value)}
-        />
-        <input 
-          type="text"
-          placeholder="Altura em (cm) Ex: 190"
-          value={altura}
-          onChange={(e)=> setAltura(e.target.value)}
-        />
-        <button>
-          Calcular
-        </button>
-      </div>
+        <input text="text" 
+               placeholder="Peso em (kg) Ex: 90" 
+               value={peso}
+               onChange={ (e) => setPeso(e.target.value)}></input>
 
-      <h2>Seu imc foi de 25, você esta com o peso ideal</h2>
+        <input text="text" 
+               placeholder="Altura em (cm) Ex: 180" 
+               value={altura}
+               onChange={ (e) => setAltura(e.target.value)}></input>
+
+        <button onClick={calcularIMC}>Calcular</button>
+      </div>
+      <h2>{mensagem}</h2>
     </div>
   )
 }
